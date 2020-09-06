14feaef33f472f4537aaef51732c02008ff73699
grenaderplus <grenaderplus@gmail.com>
2020-09-06 23:46:13 +0300

    HW_LESSON_3

---
+++ src/main/java/LESSON_3/LESSON_HW_3.java
@@ -0,0 +1,128 @@
+package LESSON_3;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class LESSON_HW_3 {
+    static Scanner sc = new Scanner(System.in);
+    static Random random = new Random();
+
+    public static void main(String[] args) {
+//          Материал урока №3
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//
+//        System.out.println(a + " + " + b + " = " + (a + b));
+//        System.out.printf("%d + %d = %d\n", a, b, a + b);
+//
+//        sc.nextLine();
+//        String name = sc.nextLine();
+//        System.out.println("Hello " + name);
+
+//        readFile("1.txt");
+
+//        int a = random.nextInt(100) + 400;
+//        int b = random.nextInt(100) - 200;
+//
+//        System.out.printf("%d + %d = %d\n", a, b, a + b);
+
+//        System.out.printf("%+-7d %+-10.2f %s %c", 12345, 1.2345, "java", 'A');
+
+//        int[] arr = factoryIntArrayFillRandom(10, 0, 100);
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println(sumArr(arr));
+//        System.out.println(countMoreNumber(arr, 56));
+
+//        System.out.println(sum(4, 5));
+//        System.out.println(sum(1, 2, 3));
+//        System.out.println(sum(1, 2, 3, 4));
+//        System.out.println(sum(1.5,3));
+//        System.out.println(sum(4,5,6,7,8,9,10));
+
+//        passwordGuessGame();
+
+//        String s = "apple";
+//        char[] chars = s.toCharArray();
+//        System.out.println(Arrays.toString(chars));
+//        System.out.println(s.charAt(2));
+
+//          Домашнее задание к уроку №3
+
+    }
+
+//    static void passwordGuessGame(){
+//        String pass = "12345";
+//        String userPass;
+//            do {
+//                System.out.println("Input Pass");
+//                userPass = sc.nextLine();
+//            } while (!pass.equals(userPass));
+//                System.out.println("Access granted");
+//    }
+//
+////    static int sum(int a, int...arr){
+////        return a + sumArr(arr);
+////    }
+////
+////    static int sum(int... arr){
+////        return sumArr(arr);
+////    }
+////
+////    static double sum(double a, double b){
+////        return a + b;
+////    }
+////
+////    static int sum(int a, int b, int c, int d) {
+////        return a + b + c + d;
+////    }
+////
+////    static int sum(int a, int b) {
+////        return a + b;
+////    }
+////
+////    static int sum(int a, int b, int c) {
+////        return a + b + c;
+////    }
+//
+//    static int countMoreNumber(int[] arr, int num) {
+//        int count = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] == num) {
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+//
+//    static int sumArr(int[] arr) {
+//        int sum = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            sum += arr[i];
+//        }
+//        return sum;
+//    }
+//
+//    static int[] factoryIntArrayFillRandom(int size, int start, int end) {
+//        int[] arr = new int[size];
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = random.nextInt(end - start) + start;
+//        }
+//        return arr;
+//    }
+
+//    static void readFile(String filename) {
+//        try (Scanner scanner = new Scanner(new FileInputStream(filename))) {
+//            while (scanner.hasNext()) {
+//                System.out.println(scanner.nextLine());
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+}
+
